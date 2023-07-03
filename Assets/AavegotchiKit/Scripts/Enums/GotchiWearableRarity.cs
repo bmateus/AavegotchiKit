3c41e6e018d1570a8d2ad2c5a83427f24fe88355
bmateus <bmateus@gmail.com>
2023-07-03 01:40:45 -0400

    Ported the wearable fetching code from js (ethers) to C# (Nethereum)

---
+++ Assets/AavegotchiKit/Scripts/Enums/GotchiWearableRarity.cs
@@ -0,0 +1,13 @@
+namespace PortalDefender.AavegotchiKit
+{
+    public enum GotchiWearableRarity : int
+    {
+        Undefined = 0,
+        Common = 1,
+        Uncommon = 2,
+        Rare = 5,
+        Legendary = 10,
+        Mythical = 20,
+        Godlike = 50,
+    }
+}
